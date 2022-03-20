860ed3172128b5c2a30eadb4bc706ec04b0f2bcc
1sses <vladzholon@gmail.com>
2022-03-20 14:56:03 +0300

    Initial commit

---
+++ src/views/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-input placeholder="Hello" v-model="text" class="input" />
+  <el-button>Hello</el-button>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      text: ''
+    }
+  }
+}
+</script>
+
+<style>
+.input {
+  width: 200px;
+  margin-right: 1rem;
+}
+</style>

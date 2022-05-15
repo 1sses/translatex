d5208aad152f6ffdb68432a8cdcb86b93fad5270
vladz <vladzholon@gmail.com>
2022-05-15 17:33:40 +0300

    get resulted row feature

--- src/algorithms/translate/getCurrentLine.js
+++ src/algorithms/translate/getCurrentLine.js
@@ -3,9 +3,7 @@ import getRowType from '@/algorithms/analyse/getRowType'
 
 export default function getCurrentLine (line1, line2, preset, mode) {
   const l1 = line1.trim()
-  const l2 = line2.trim()
   const rowType = getRowType(l1, preset)
-  // is it working?
   const startTabs = line1.match(/^[\s\uFEFF\xA0]+/g) ?? ''
   switch (preset) {
     case 'renpy':
@@ -18,8 +16,6 @@ export default function getCurrentLine (line1, line2, preset, mode) {
           return startTabs + lineMatch + '"'
         } else if (l1.startsWith('"')) return startTabs + '""'
         else return line1
-      } else {
-        // code here for automatic mode
       }
       return line2
     default:

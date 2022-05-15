4426c3a0a1f20dbe7b54473ae10c747693d93801
vladz <vladzholon@gmail.com>
2022-05-15 18:11:28 +0300

    Yandex Translate working

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,21 @@
 <template>
   <el-header class="header">
     <h1>TranslateX</h1>
-    <el-tabs class="tabs" v-model="activeName" @tab-click="handleClick">
-      <el-tab-pane label="Translate" name="/">Translate file</el-tab-pane>
-      <el-tab-pane label="Analyse" name="/analyse">File analysis</el-tab-pane>
-      <el-tab-pane label="Compare" name="/compare">Compare files</el-tab-pane>
-    </el-tabs>
+    <el-row align="middle">
+      <TranslationApiDrawer style="margin-right: 50px" />
+      <el-tabs class="tabs" v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="Translate" name="/">Translate file</el-tab-pane>
+        <el-tab-pane label="Analyse" name="/analyse">File analysis</el-tab-pane>
+        <el-tab-pane label="Compare" name="/compare">Compare files</el-tab-pane>
+      </el-tabs>
+    </el-row>
   </el-header>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import TranslationApiDrawer from '@/components/TranslationApiDrawer'
 
 const activeName = ref(location.pathname)
 
---
+++ src/components/TranslationApiDrawer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <el-button type="primary" @click="drawer = true" size="large" text bg>
+      <el-image :src="translate" />
+    </el-button>
+    <el-drawer
+      v-model="drawer"
+      direction="rtl"
+    >
+      <template #title>
+        <h2>Yandex.Translate</h2>
+      </template>
+      <h3>Use Yandex translation API to translate your files!</h3>
+      <h3>Yandex.Cloud console and API-key required.</h3>
+      <template #footer>
+        <h3>Powered by <el-link type="primary" href="https://translate.yandex.ru">Yandex.Translator</el-link></h3>
+      </template>
+    </el-drawer>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import translate from '@/translate.svg'
+
+const drawer = ref(false)
+const key = ref(localStorage.getItem('yandex-translate-api-key') || '')
+</script>
+
+<style scoped>
+h3 a {
+  text-decoration: none;
+  color: #409EFF;
+}
+</style>
